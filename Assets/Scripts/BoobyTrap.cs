b4740af9a07d305592d7a947272287e41ba82d37
MrPabloVS <pablovazquezsoldano@gmail.com>
2022-11-07 20:29:24 -0300

    BoobyTrapRayDraw

--- Assets/Scripts/BoobyTrap.cs
+++ Assets/Scripts/BoobyTrap.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class BoobyTrap : MonoBehaviour
 {
     public float maxDistance = 50f;
     public LayerMask layerToCollide;
+    public UnityEngine.Vector3 direction;
+    public GameObject salida;
     private bool Active = true;
 
 
@@ -27,11 +30,22 @@ public class BoobyTrap : MonoBehaviour
         RaycastHit hit;
         Ray ray;
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance, layerToCollide))
-        {
-            Debug.Log(hit.distance);
+        // if (Physics.Raycast(transform.position, direction, out hit, maxDistance, layerToCollide))
+        // {
+        //     Debug.Log(hit.distance);
 
+        // }
+        if (Physics.Raycast(salida.transform.position, direction, out hit, Mathf.Infinity, layerToCollide))
+        {
+            Debug.DrawRay(salida.transform.position, direction * hit.distance, Color.yellow);
+            Debug.Log(hit.collider);
         }
+        else
+        {
+            Debug.DrawRay(salida.transform.position, direction * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    
 
     }
 
